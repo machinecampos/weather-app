351dc15702a79ddc68cbca133ba859507153f87b
Eduardo Campos <edumachadocampos@gmail.com>
2024-08-18 22:13:05 -0300

    More code

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ type Props = {}
 export default function Navbar({}: Props) {
   return (
     <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
-        <div className="h-[80px] "></div>
+        <div className="h-[80px] w-full flex justify-between items-center max-w-7x1 px-3 mx-auto"></div>
         Navbar
     </nav>
   )
