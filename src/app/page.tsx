496d3ca77231224bfbb0be873ddf8992761df2a5
Eduardo Campos <edumachadocampos@gmail.com>
2024-10-22 01:11:06 -0300

    More code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,47 @@
+"use client";
 import Navbar from "@/components/Navbar"
-import Image from "next/image";
+import Image from "next/image"
+import axios from "axios"
+import {useState} from "react"
+import {BsSearch} from "react-icons/bs"
 
 export default function Home() {
+
+  const[city, setCity] = useState('');
+  const[weather, setWeather] = useState({});
+  const[loading, setLoading] = useState(false);
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=dubai&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`
+
+  const fetchWeather = (e: any) => {
+    e.preventDefault()
+    setLoading(true)
+    axios.get(url).then((response) => {
+      setWeather(response.data)
+      console.log(response.data)
+    })
+    setCity('')
+    setLoading(false)
+  }
+
   return (
-    <div className="flex flex-col gap-4 bg-gry-100 min-h-screen">
+    <div>
       <Navbar />
+
+      <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/30 z-[1]" />
+      <Image src="https://images.unsplash.com/photo-1592210454359-9043f067919b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      fill className="object-cover"
+      alt="Landing photo" />
+
+      <div className="relative flex justify-between items-center max-w-[500px] w-full">
+        <form>
+          <div>
+            <input type="text" placeholder="Search city" />
+          </div>
+          <button onClick={fetchWeather}><BsSearch /></button>
+        </form>
+      </div>
+
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,11 +4,10 @@ type Props = {}
 
 export default function Navbar({}: Props) {
   return (
-    <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
-        <div className="h-[80px] w-full flex justify-between items-center max-w-7x1 px-3 mx-auto">
-          <p className="flex items-center justify-center gap-2">
-          <h2 className="text-gray-500 text-3x1">Weather</h2>
-          </p>
+    <nav className="">
+        <div className="">
+          
+          <h2 className="">Weather</h2>
         </div>
         Navbar
     </nav>
