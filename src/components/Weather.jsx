6afd5846776fd6e8d88d4b5e336ac122725fde71
Eduardo Campos <edumachadocampos@gmail.com>
2025-02-10 23:47:19 -0300

    More code

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,9 +1,21 @@
 import React from "react"
+import Image from "next/image"
 
 const Weather = ({data}) => {
     console.log(data)
     return (
-        <div>Weather</div>
+        <div>
+            <div>
+                <div>
+                    <Image 
+                        src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} 
+                        alt="/" 
+                        width="100"
+                        height="100"
+                    />
+                </div>
+            </div>
+        </div>
     )
 }
 
