1e6d604db3815b4c1a188b24165b131f5a27b904
Eduardo Campos <edumachadocampos@gmail.com>
2025-02-16 23:59:25 -0300

    More code

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -13,6 +13,7 @@ const Weather = ({data}) => {
                         width="100"
                         height="100"
                     />
+                    <p>{data.weather[0].main}</p>
                 </div>
             </div>
         </div>
