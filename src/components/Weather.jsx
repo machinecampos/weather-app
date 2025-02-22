2661607e8e3cca84f683dd51763b225e20c847b1
Eduardo Campos <edumachadocampos@gmail.com>
2025-02-21 23:43:08 -0300

    More code

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -5,8 +5,8 @@ const Weather = ({data}) => {
     console.log(data)
     return (
         <div className="relative flex flex-col justify-between max-w-[500px] w-full h-[90vh] m-auto p-4 text-gray-300 z-10">
-            <div>
-                <div>
+            <div className="relative flex justify-between pt-12">
+                <div className="flex flex-col items-center">
                     <Image 
                         src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} 
                         alt="/" 
